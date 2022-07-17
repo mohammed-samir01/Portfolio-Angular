16291d860b3272d79453fd60c899c06a4e88967c
mohamed_samir <86778943+mohammed-samir01@users.noreply.github.com>
2022-07-17 07:11:59 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { SidbarComponent } from './sidbar/sidbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MyEducationComponent } from './my-education/my-education.component';
+import { MyPortfolioComponent } from './my-portfolio/my-portfolio.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { HeadComponent } from './head/head.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    SidbarComponent,
+    FooterComponent,
+    AboutMeComponent,
+    MyEducationComponent,
+    MyPortfolioComponent,
+    ContactMeComponent,
+    HeadComponent
   ],
   imports: [
     BrowserModule
